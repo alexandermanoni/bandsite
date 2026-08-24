c43fd32a90e68fac32e6f6f40f5d509b2b6ff384
Your Name <you@example.com>
2026-08-24 16:36:57 -0400

    Updated backend again to hopefully fix reftok

--- newbackend/http/server.go
+++ newbackend/http/server.go
@@ -256,15 +256,25 @@ func (s *Server) loginUser(c *gin.Context) {
 	}
 
 	// return refresh token as HttpsOnly cookie
-	c.SetCookie(
-		"refresh_token",
-		refreshtoken,
-		60*60*24*30, // 30 days
-		"/auth",
-		"setlistcreationutility.com",
-		true, // secure
-		true, // http only
-	)
+	cookie := &http.Cookie{
+		Name:     "refresh_token",
+		Value:    refreshtoken,
+		Path:     "/auth",
+		MaxAge:   60 * 60 * 24 * 30,
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteNoneMode,
+	}
+	http.SetCookie(c.Writer, cookie)
+	// c.SetCookie(
+	// 	"refresh_token",
+	// 	refreshtoken,
+	// 	60*60*24*30, // 30 days
+	// 	"/auth",
+	// 	"setlistcreationutility.com",
+	// 	true, // secure
+	// 	true, // http only
+	// )
 
 	// return JWT access token
 	c.JSON(http.StatusOK, gin.H{
